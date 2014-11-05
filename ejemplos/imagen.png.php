7db071a4dabfa14ffe83a890fd403f9c979bf305
Jose Alexis Correa valencia <jalexiscv@gmail.com>
2014-11-05 03:45:38 -0500

    Update imagen.png.php

--- ejemplos/imagen.png.php
+++ ejemplos/imagen.png.php
@@ -1,22 +1,25 @@
 <?php
-include("../librerias/Sprites.class.php");
+$root=(!isset($root))?"../":$root;
+include($root."librerias/Sprites.class.php");
+$imagenes=$root."imagenes/";
+$ejemplos=$root."ejemplos/";
 $sprite = new Sprites("png");
-$sprite->agregar("advertencia.png", "i100x100_advertencia");
-$sprite->agregar("ayuda.png", "i100x100_ayuda");
-$sprite->agregar("buscar.png", "i100x100_buscar");
-$sprite->agregar("confirmado.png", "i100x100_confirmado");
-$sprite->agregar("confirmar.png", "i100x100_confirmar");
-$sprite->agregar("documento.png", "i100x100_documento");
-$sprite->agregar("eliminar.png", "i100x100_eliminar");
-$sprite->agregar("factura.png", "i100x100_factura");
-$sprite->agregar("georeferencia.png", "i100x100_georeferencia");
-$sprite->agregar("medidor.png", "i100x100_medidor");
-$sprite->agregar("requisitos.png", "i100x100_requisitos");
-$sprite->agregar("seguro.png", "i100x100_seguro");
+$sprite->agregar($imagenes."advertencia.png","advertencia");
+$sprite->agregar($imagenes."ayuda.png","ayuda");
+$sprite->agregar($imagenes."buscar.png","buscar");
+$sprite->agregar($imagenes."confirmado.png","confirmado");
+$sprite->agregar($imagenes."confirmar.png","confirmar");
+$sprite->agregar($imagenes."documento.png","documento");
+$sprite->agregar($imagenes."eliminar.png","eliminar");
+$sprite->agregar($imagenes."factura.png","factura");
+$sprite->agregar($imagenes."georeferencia.png","georeferencia");
+$sprite->agregar($imagenes."medidor.png","medidor");
+$sprite->agregar($imagenes."requisitos.png","requisitos");
+$sprite->agregar($imagenes."seguro.png","seguro");
 $errores= $sprite->errores();
 if(!empty($errores)){
 	foreach($errores as $error){
-		echo "<p>".$error."</p>";
+		echo("<p>".$error."</p>");
 	}
 }else{
 	$sprite->imagen();	
